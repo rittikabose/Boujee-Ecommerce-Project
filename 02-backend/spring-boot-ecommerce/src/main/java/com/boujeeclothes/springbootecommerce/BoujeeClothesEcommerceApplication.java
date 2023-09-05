5cde4e36e037e22fbceb9eb0c797dfc9bcd58b7d
rittikabose <106527356+rittikabose@users.noreply.github.com>
2023-09-05 20:56:39 +0530

    Add files via upload

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/boujeeclothes/springbootecommerce/BoujeeClothesEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.boujeeclothes.springbootecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoujeeClothesEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoujeeClothesEcommerceApplication.class, args);
+	}
+
+}
---
+++ 02-backend/spring-boot-ecommerce/src/test/java/com/boujeeclothes/springbootecommerce/BoujeeClothesEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boujeeclothes.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoujeeClothesEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
